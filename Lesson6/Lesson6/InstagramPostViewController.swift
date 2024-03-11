e0f4129f9e3bf77d0f2e024861885c0c6d73cb5c
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-03-11 10:40:32 +0100

    Add Stack Views

---
+++ Lesson6/Lesson6/InstagramPostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson6
+//
+//  Created by Алина Власенко on 07.03.2024.
+//
+
+import UIKit
+
+class InstagramPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
